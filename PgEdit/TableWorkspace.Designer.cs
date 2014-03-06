79156678a0d8f1e04811ddc3c430c5b134c3de3f
danshilevich <Dmitriy.Anshilevich@softline.ru>
2014-03-06 18:12:37 +0400

    add data table filters

---
+++ PgEdit/TableWorkspace.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace PgEdit
+{
+    partial class TableWorkspace
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.bsTable = new System.Windows.Forms.BindingSource(this.components);
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.bsTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bsTable
+            // 
+            this.bsTable.AllowNew = false;
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvData.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvData.AutoGenerateColumns = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.DataSource = this.bsTable;
+            this.dgvData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvData.Location = new System.Drawing.Point(0, 0);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.RowHeadersVisible = false;
+            this.dgvData.Size = new System.Drawing.Size(826, 489);
+            this.dgvData.TabIndex = 2;
+            this.dgvData.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dgvData_DataBindingComplete);
+            // 
+            // TableWorkspace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dgvData);
+            this.Name = "TableWorkspace";
+            this.Size = new System.Drawing.Size(826, 489);
+            ((System.ComponentModel.ISupportInitialize)(this.bsTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.BindingSource bsTable;
+    }
+}
---
+++ PgEdit/TableWorkspace.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataGridViewAutoFilter;
+
+namespace PgEdit
+{
+    public partial class TableWorkspace : UserControl
+    {
+        public TableWorkspace()
+        {
+            InitializeComponent();
+
+            dgvData.AutoGenerateColumns = true;
+        }
+
+        public Object DataSource
+        {
+            get
+            {
+                return bsTable.DataSource;
+            }
+            set
+            {
+                bsTable.DataSource = value;
+            }
+        }
+
+        public string DataMember
+        {
+            get
+            {
+                return bsTable.DataMember;
+            }
+            set
+            {
+                bsTable.DataMember = value;
+            }
+        }
+
+        private void SetupFilters()
+        {
+            if (dgvData.DataSource != null)
+            {
+                bool resize = false;
+
+                foreach (DataGridViewColumn col in dgvData.Columns)
+                {
+                    if (!(col.HeaderCell is DataGridViewAutoFilterColumnHeaderCell))
+                    {
+                        col.HeaderCell = new DataGridViewAutoFilterColumnHeaderCell(col.HeaderCell);
+                        resize = true;
+                    }
+                }
+
+                if (resize)
+                {
+                    dgvData.AutoResizeColumns();
+                }
+            }
+        }
+
+        private void dgvData_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            SetupFilters();
+        }
+    }
+}
