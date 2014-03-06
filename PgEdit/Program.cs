922ef365b0df8de1cc3084ad953472288145158f
danshilevich <Dmitriy.Anshilevich@softline.ru>
2014-03-06 19:38:14 +0400

    add TODO

--- PgEdit/Program.cs
+++ PgEdit/Program.cs
@@ -20,6 +20,7 @@ namespace PgEdit
             // TODO navigation: clicking by foreight key cell send user to row with primary key
             // TODO navigation: clicking by primary key cell opens menu with depend tables. selecting table send user to table with dependent rows filtered
             // TODO navigation: forward and backward buttons, support mouse additional buttons
+            // TODO SQL editor
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
