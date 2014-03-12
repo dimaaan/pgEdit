7c6a38886a704265fe9ac6f4f5cea889558c851f
danshilevich <Dmitriy.Anshilevich@softline.ru>
2014-03-12 20:44:40 +0400

    move TODOs to github issues

--- PgEdit/Program.cs
+++ PgEdit/Program.cs
@@ -14,14 +14,6 @@ namespace PgEdit
         [STAThread]
         static void Main()
         {
-            // TODO refresh table data button
-            // TODO table schema, column type. If enumeration - display values in tooltip
-            // TODO what table currently open?
-            // TODO table Fields: more details
-            // TODO navigation: clicking by foreight key cell send user to row with primary key
-            // TODO navigation: clicking by primary key cell opens menu with depend tables. selecting table send user to table with dependent rows filtered
-            // TODO navigation: forward and backward buttons, support mouse additional buttons
-            // TODO SQL editor
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
