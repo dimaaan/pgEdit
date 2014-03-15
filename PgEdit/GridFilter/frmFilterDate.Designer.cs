bc996fa0e147af68777e46e1d5d15dc1657ca613
Dmitry Anshilevich <dimaaan@gmail.com>
2014-03-15 15:58:23 +0400

    add frmFilterDate

---
+++ PgEdit/GridFilter/frmFilterDate.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace PgEdit.GridFilter
+{
+    partial class frmFilterDate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.lblFiledName = new System.Windows.Forms.Label();
+            this.cmbOperand = new System.Windows.Forms.ComboBox();
+            this.dtpValue = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(198, 131);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 69;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(279, 131);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 70;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(12, 10);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(118, 13);
+            this.lblPrompt.TabIndex = 68;
+            this.lblPrompt.Text = "Показать записи где:";
+            // 
+            // lblFiledName
+            // 
+            this.lblFiledName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFiledName.AutoEllipsis = true;
+            this.lblFiledName.Location = new System.Drawing.Point(12, 23);
+            this.lblFiledName.Name = "lblFiledName";
+            this.lblFiledName.Size = new System.Drawing.Size(342, 23);
+            this.lblFiledName.TabIndex = 71;
+            this.lblFiledName.Text = "field name";
+            this.lblFiledName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cmbOperand
+            // 
+            this.cmbOperand.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperand.FormattingEnabled = true;
+            this.cmbOperand.Location = new System.Drawing.Point(15, 49);
+            this.cmbOperand.Name = "cmbOperand";
+            this.cmbOperand.Size = new System.Drawing.Size(133, 21);
+            this.cmbOperand.TabIndex = 72;
+            // 
+            // dtpValue
+            // 
+            this.dtpValue.Location = new System.Drawing.Point(154, 49);
+            this.dtpValue.Name = "dtpValue";
+            this.dtpValue.Size = new System.Drawing.Size(200, 20);
+            this.dtpValue.TabIndex = 73;
+            // 
+            // frmFilterDate
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(366, 165);
+            this.Controls.Add(this.dtpValue);
+            this.Controls.Add(this.cmbOperand);
+            this.Controls.Add(this.lblFiledName);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblPrompt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmFilterDate";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Фильтр по дате";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.Label lblFiledName;
+        private System.Windows.Forms.ComboBox cmbOperand;
+        private System.Windows.Forms.DateTimePicker dtpValue;
+    }
+}
