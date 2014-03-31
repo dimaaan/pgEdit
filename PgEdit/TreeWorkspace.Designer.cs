98436bc2453a16190b4794f9e72f047c8f534b3f
Dmitry Anshilevich <dimaaan@gmail.com>
2014-03-31 23:32:33 +0400

    separate TreeView into UserControl - TreeWorkspace

---
+++ PgEdit/TreeWorkspace.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace PgEdit
+{
+    partial class TreeWorkspace
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmsServer = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiRemoveServer = new System.Windows.Forms.ToolStripMenuItem();
+            this.ilTreeView = new System.Windows.Forms.ImageList(this.components);
+            this.tvTree = new System.Windows.Forms.TreeView();
+            this.cmsServer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmsServer
+            // 
+            this.cmsServer.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiRemoveServer});
+            this.cmsServer.Name = "cmsStructure";
+            this.cmsServer.Size = new System.Drawing.Size(128, 26);
+            // 
+            // tsmiRemoveServer
+            // 
+            this.tsmiRemoveServer.Name = "tsmiRemoveServer";
+            this.tsmiRemoveServer.Size = new System.Drawing.Size(127, 22);
+            this.tsmiRemoveServer.Text = "Удалить...";
+            this.tsmiRemoveServer.Click += new System.EventHandler(this.tsmiRemoveServer_Click);
+            // 
+            // ilTreeView
+            // 
+            this.ilTreeView.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.ilTreeView.ImageSize = new System.Drawing.Size(16, 16);
+            this.ilTreeView.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // tvTree
+            // 
+            this.tvTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvTree.ImageIndex = 0;
+            this.tvTree.ImageList = this.ilTreeView;
+            this.tvTree.Location = new System.Drawing.Point(0, 0);
+            this.tvTree.Name = "tvTree";
+            this.tvTree.SelectedImageIndex = 0;
+            this.tvTree.Size = new System.Drawing.Size(223, 434);
+            this.tvTree.TabIndex = 1;
+            this.tvTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvTree_AfterSelect);
+            this.tvTree.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvTree_NodeMouseDoubleClick);
+            this.tvTree.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tvTree_KeyPress);
+            // 
+            // TreeWorkspace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tvTree);
+            this.Name = "TreeWorkspace";
+            this.Size = new System.Drawing.Size(223, 434);
+            this.Load += new System.EventHandler(this.TreeWorkspace_Load);
+            this.cmsServer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip cmsServer;
+        private System.Windows.Forms.ToolStripMenuItem tsmiRemoveServer;
+        private System.Windows.Forms.ImageList ilTreeView;
+        private System.Windows.Forms.TreeView tvTree;
+    }
+}
--- PgEdit/frmMain.cs
+++ PgEdit/frmMain.cs
@@ -19,251 +19,17 @@ namespace PgEdit
     {
         private Universe universe;
 
-        private const string IMAGE_KEY_SERVER = "server";
-        private const string IMAGE_KEY_DATABASE_CONNECTED = "dbConnected";
-        private const string IMAGE_KEY_DATABASE_DISCONNECTED = "dbDisconnected";
-        private const string IMAGE_KEY_SCHEMA = "schema";
-        private const string IMAGE_KEY_TABLE = "table";
-        private const string IMAGE_KEY_TABLE_OPEN = "table-open";
-
         public frmMain()
         {
             InitializeComponent();
         }
 
-        public void FillTreeView(Universe universe)
-        {
-            tvTree.SuspendLayout();
-            tvTree.Nodes.Clear();
-
-            foreach (Server server in universe.Servers)
-            {
-                TreeNode nodeHost = ServerToNode(server);
-                tvTree.Nodes.Add(nodeHost);
-            }
-
-            tvTree.ExpandAll();
-            tvTree.ResumeLayout();
-        }
-
-        private void OpenDatabase(TreeNode dbNode) {
-            Database db = (Database)dbNode.Tag;
-            Server server = (Server)dbNode.Parent.Tag;
-
-            using (NpgsqlConnection connection = ConnectionService.GetConnection(server, db))
-            {
-                connection.Open();
-                db.Schemas = DatabaseService.fetchAllSchemasWithTables(connection);
-                foreach (DataSet schema in db.Schemas)
-                {
-                    db.Columns[schema.DataSetName] = new Dictionary<string, List<Column>>();
-                }
-            }
-
-            FillTreeViewWithTables(dbNode);
-
-            
-            dbNode.ImageKey = IMAGE_KEY_DATABASE_CONNECTED;
-            dbNode.SelectedImageKey = IMAGE_KEY_DATABASE_CONNECTED;
-            db.IsOpen = true;
-            RefreshMenu();
-        }
-
-        private void CloseDatabase(TreeNode dbNode)
-        {
-            Database db = (Database)dbNode.Tag;
-
-            if (db.IsOpen)
-            {
-                ucTable.DataSource = null;
-                ucTable.Tag = null;
-                dgvColumns.DataSource = null;
-                db.Schemas = null;
-                db.IsOpen = false;
-                dbNode.Nodes.Clear();
-                dbNode.ImageKey = IMAGE_KEY_DATABASE_DISCONNECTED;
-                dbNode.SelectedImageKey = IMAGE_KEY_DATABASE_DISCONNECTED;
-                RefreshMenu();
-            }
-        }
-
-        private void CloseDatabasesOfServer(TreeNode serverNode)
-        {
-            foreach (TreeNode dbNode in serverNode.Nodes)
-            {
-                CloseDatabase(dbNode);
-            }
-        }
-
-        /// <summary>
-        /// Return database node by it subnodes
-        /// </summary>
-        public static TreeNode GetSelectedDBNode(TreeNode node)
-        {
-            TreeNode res;
-
-            if (node.Tag is Server)
-            {
-                res = null;
-            }
-            else if (node.Tag is DataTable || node.Tag is DataSet)
-            {
-                res = GetSelectedDBNode(node.Parent);
-            }
-            else if (node.Tag is Database)
-            {
-                res = node;
-            }
-            else
-            {
-                throw new InvalidOperationException("Invalid tree node type: " + node.Tag.GetType().ToString());
-            }
-
-            return res;
-        }
-
-        private TreeNode ServerToNode(Server server)
-        {
-            TreeNode nodeHost = new TreeNode()
-            {
-                Text = server.Address,
-                Tag = server,
-                ImageKey = IMAGE_KEY_SERVER,
-                SelectedImageKey = IMAGE_KEY_SERVER,
-                ContextMenuStrip = cmsServer
-            };
-
-            foreach (Database db in server.Databases)
-            {
-                TreeNode nodeDB = new TreeNode()
-                {
-                    Text = db.Name,
-                    Tag = db,
-                    ImageKey = IMAGE_KEY_DATABASE_DISCONNECTED,
-                    SelectedImageKey = IMAGE_KEY_DATABASE_DISCONNECTED,
-                };
-                nodeHost.Nodes.Add(nodeDB);
-            }
-            return nodeHost;
-        }
-
-        private void RemoveServer(TreeNode selectedNode)
-        {
-            string msg = "Настройки подключения к серверу и его базам данных будут удалены.{0}Продолжить?";
-            var res = MessageBox.Show(
-                String.Format(msg, Environment.NewLine),
-                null,
-                MessageBoxButtons.YesNo,
-                MessageBoxIcon.Question,
-                MessageBoxDefaultButton.Button2);
-
-            if (res == DialogResult.Yes)
-            {
-                Server server = (Server)selectedNode.Tag;
-
-                SuspendLayout();
-                CloseDatabasesOfServer(selectedNode);
-                universe.Servers.Remove(server);
-                ConnectionService.CloseSshTunnel(server);
-                ConnectionService.Save(universe);
-                selectedNode.Remove();
-                ResumeLayout();
-            }
-        }
-
-        private void ShowTable(TreeNode node)
-        {
-            DataTable table = (DataTable)node.Tag;
-            TreeNode dbNode = GetSelectedDBNode(node);
-            Database db = (Database)dbNode.Tag;
-            Server server = (Server)dbNode.Parent.Tag;
-            List<Column> columns;
-
-            using (NpgsqlConnection connection = ConnectionService.GetConnection(server, db))
-            {
-                connection.Open();
-                DatabaseService.fetchTableByName(connection, table);
-                columns = DatabaseService.fetchTableColumns(connection, table.DataSet.DataSetName, table.TableName);
-                db.Columns[table.DataSet.DataSetName][table.TableName] = columns;
-            }
-
-
-            // hilight image of selected table
-            TreeNode prevSelected = (TreeNode)ucTable.Tag;
-            if (prevSelected != null)
-            {
-                prevSelected.ImageKey = IMAGE_KEY_TABLE;
-                prevSelected.SelectedImageKey = IMAGE_KEY_TABLE;
-            }
-            node.ImageKey = IMAGE_KEY_TABLE_OPEN;
-            node.SelectedImageKey = IMAGE_KEY_TABLE_OPEN;
-
-
-            dgvColumns.DataSource = columns;
-            ucTable.DataSource = table;
-            ucTable.Tag = node;
-        }
-
-        public static void FillTreeViewWithTables(TreeNode dbNode)
-        {
-            Database db = (Database)dbNode.Tag;
-            List<TreeNode> nodes = new List<TreeNode>();
-
-            foreach (DataSet schema in db.Schemas)
-            {
-                TreeNode schemaNode = new TreeNode()
-                {
-                    Text = schema.DataSetName,
-                    Tag = schema,
-                    ImageKey = IMAGE_KEY_SCHEMA,
-                    SelectedImageKey = IMAGE_KEY_SCHEMA
-                };
-                nodes.Add(schemaNode);
-
-                foreach (DataTable table in schema.Tables)
-                {
-                    TreeNode tableNode = new TreeNode()
-                    {
-                        Text = table.TableName,
-                        Tag = table,
-                        ImageKey = IMAGE_KEY_TABLE,
-                        SelectedImageKey = IMAGE_KEY_TABLE
-                    };
-
-                    schemaNode.Nodes.Add(tableNode);
-                }
-            }
-
-            dbNode.Nodes.AddRange(nodes.ToArray());
-            dbNode.ExpandAll();
-        }
-
         private void RefreshMenu()
         {
-            TreeNode dbNode = GetSelectedDBNode(tvTree.SelectedNode);
+            Database selDb = ucTree.SelectedDatabase;
 
-            tsmiConnect.Enabled = dbNode != null && !((Database)dbNode.Tag).IsOpen;
-            tsmiDisconnect.Enabled = dbNode != null && ((Database)dbNode.Tag).IsOpen;
-        }
-
-        private void OnSelectTreeNode(TreeNode node)
-        {
-            if (node.Tag != null)
-            {
-                if (node.Tag is Database)
-                {
-                    Database db = (Database)node.Tag;
-
-                    if (!db.IsOpen)
-                    {
-                        OpenDatabase(node);
-                    }
-                }
-                else if (node.Tag is DataTable)
-                {
-                    ShowTable(node);
-                }
-            }
+            tsmiConnect.Enabled = selDb != null && !selDb.IsOpen;
+            tsmiDisconnect.Enabled = selDb != null && selDb.IsOpen;
         }
 
         private void Shutdown()
@@ -281,16 +47,10 @@ namespace PgEdit
         {
             Icon = Resources.logo;
 
-            ilTreeView.Images.Add(IMAGE_KEY_SERVER, Resources.computersystemproduct);
-            ilTreeView.Images.Add(IMAGE_KEY_DATABASE_CONNECTED, Resources.Database_Active_icon);
-            ilTreeView.Images.Add(IMAGE_KEY_DATABASE_DISCONNECTED, Resources.Database_Inactive_icon);
-            ilTreeView.Images.Add(IMAGE_KEY_SCHEMA, Resources.Schema_16xLG);
-            ilTreeView.Images.Add(IMAGE_KEY_TABLE, Resources.Table_748);
-            ilTreeView.Images.Add(IMAGE_KEY_TABLE_OPEN, Resources.Table_748_Opened);
-
             universe = ConnectionService.Load();
 
-            FillTreeView(universe);
+            ucTree.Universe = universe;
+            ucTree.FillTreeView();
         }
 
         private void tsmiNewConnection_Click(object sender, EventArgs e)
@@ -301,7 +61,7 @@ namespace PgEdit
             {
                 ConnectionService.MergeServer(universe.Servers, dlg.Server);
 
-                FillTreeView(universe);
+                ucTree.FillTreeView();
                 
                 ConnectionService.Save(universe);
             }
@@ -309,14 +69,12 @@ namespace PgEdit
 
         private void tsmiConnect_Click(object sender, EventArgs e)
         {
-            OpenDatabase(tvTree.SelectedNode);
+            ucTree.OpenSelectedDatabase();
         }
 
         private void tsmiDisconnect_Click(object sender, EventArgs e)
         {
-            TreeNode dbNode = GetSelectedDBNode(tvTree.SelectedNode);
-
-            CloseDatabase(dbNode);
+            ucTree.CloseSelectedDatabase();
         }
 
         private void tsmiAbout_Click(object sender, EventArgs e)
@@ -346,46 +104,34 @@ namespace PgEdit
             Shutdown();
         }
 
-        private void tvTree_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
-        {
-            if (e.Button == System.Windows.Forms.MouseButtons.Left)
-            {
-                OnSelectTreeNode(e.Node);
-            }
-        }
-
-        private void tvTree_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar == Convert.ToChar(Keys.Enter) &&
-                tvTree.SelectedNode != null)
-            {
-                OnSelectTreeNode(tvTree.SelectedNode);
-                e.Handled = true;
-            }
-        }
-
-        private void tvTree_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-            RefreshMenu();            
-        }
-
         private void ucTable_DataSourceNeedRefresh()
         {
-            TreeNode node = (TreeNode) ucTable.Tag; // selected node
-
-            ShowTable(node);
+            ucTree.ShowTable(ucTree.OpenedTableNode);
         }
 
-        private void tsmiRemoveServer_Click(object sender, EventArgs e)
+        private void ucTree_DatabaseOpened()
         {
-            // due to strane behavior we can't use TreeView.SelectedNode here to find out TreeNode for witch context menu is opened
-            var hitTest = tvTree.HitTest(tvTree.PointToClient(new Point(cmsServer.Left, cmsServer.Top)));
-            var selectedNode = hitTest.Node;
+            RefreshMenu();
+        }
 
-            if (selectedNode != null)
-            {
-                RemoveServer(selectedNode);
-            }
+        private void ucTree_DatabaseClosed()
+        {
+            ucTable.DataSource = null;
+            ucTable.Tag = null;
+            dgvColumns.DataSource = null;
+            RefreshMenu();
+        }
+
+        private void ucTree_TableOpened(DataTable table)
+        {
+            List<Column> columns = (List<Column>) table.ExtendedProperties[Database.TABLE_PROPERTY_COLUMNS];
+            dgvColumns.DataSource = columns;
+            ucTable.DataSource = table;
+        }
+
+        private void ucTree_SelectionChanged()
+        {
+            RefreshMenu();
         }
     }
 }
