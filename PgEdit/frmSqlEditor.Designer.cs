e4dbe37b0f2f6b7b9babd9078c295ef21000cb44
Dmitry Anshilevich <dimaaan@gmail.com>
2014-10-24 11:53:40 +0400

    sql editor: switch font to Consolas, 10

--- PgEdit/frmSqlEditor.Designer.cs
+++ PgEdit/frmSqlEditor.Designer.cs
@@ -71,6 +71,7 @@
             // 
             this.txtSqlExpr.AcceptsTab = true;
             this.txtSqlExpr.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSqlExpr.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.txtSqlExpr.Location = new System.Drawing.Point(0, 0);
             this.txtSqlExpr.Multiline = true;
             this.txtSqlExpr.Name = "txtSqlExpr";
